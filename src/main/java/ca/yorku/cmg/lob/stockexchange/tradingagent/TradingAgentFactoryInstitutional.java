a7396e2910f78c3c791d34762a732e2e3a2b7b69
Danielk222 <dk22@my.yorku.ca>
2025-03-19 17:06:21 -0400

    Update TradingAgentFactoryInstitutional.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentFactoryInstitutional.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentFactoryInstitutional.java
@@ -2,17 +2,15 @@ package ca.yorku.cmg.lob.stockexchange.tradingagent;
 
 import ca.yorku.cmg.lob.stockexchange.StockExchange;
 import ca.yorku.cmg.lob.stockexchange.events.NewsBoard;
-import ca.yorku.cmg.lob.trader.Trader;
-
+import ca.yorku.cmg.lob.trader.Trader; 
 
 public class TradingAgentFactoryInstitutional extends AbstractTradingAgentFactory {
   public TradingAgent createConservativeAgent() {
-        return new InstitutionalTradingAgent(new ConservativeTradingStrategy());
+        return new TradingAgentInstitutional(new ConservativeTradingStrategy());
     }
 
-    @Override
     public TradingAgent createAggressiveAgent() {
-        return new InstitutionalTradingAgent(new AggressiveTradingStrategy());
+        return new TradingAgentInstitutional(new AggressiveTradingStrategy());
     }
 
 } 
