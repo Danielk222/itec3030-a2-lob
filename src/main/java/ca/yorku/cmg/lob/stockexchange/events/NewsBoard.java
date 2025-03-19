40edf5c2ef88de5a2228bc048391f5c34c86daa5
Danielk222 <dk22@my.yorku.ca>
2025-03-18 20:36:37 -0400

    Update NewsBoard.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/events/NewsBoard.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/events/NewsBoard.java
@@ -115,7 +115,10 @@ public class NewsBoard {
 	public void registerObserver(INewsobserver o) {
 		observers.add(o)
 	}
-	
+	 public void removeObserver(INewsObserver o) {
+        observers.remove(o);
+	}
+		 
 	public void notifyObservers(Event event) {
 		for (INewsObserver observer: observers) {
 			observer.update(event)
