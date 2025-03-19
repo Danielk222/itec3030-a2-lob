d479b45f891e6ca59c67ae0ecbbacb7d252cb067
Danielk222 <dk22@my.yorku.ca>
2025-03-19 17:01:09 -0400

    Update TradingAgentFactoryInstitutional.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentFactoryInstitutional.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentFactoryInstitutional.java
@@ -2,4 +2,14 @@ package
 
 import
 
-public class TradingAgentFactoryInstitutional implements 
+public class TradingAgentFactoryInstitutional extends AbstractTradingAgentFactory {
+  public TradingAgent createConservativeAgent() {
+        return new InstitutionalTradingAgent(new ConservativeTradingStrategy());
+    }
+
+    @Override
+    public TradingAgent createAggressiveAgent() {
+        return new InstitutionalTradingAgent(new AggressiveTradingStrategy());
+    }
+
+} 
