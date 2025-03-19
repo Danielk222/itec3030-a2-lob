72643a96081a4ba9d7cfedbc4531149512749969
Danielk222 <dk22@my.yorku.ca>
2025-03-19 16:28:14 -0400

    Update TradingAgentRetail.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentRetail.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentRetail.java
@@ -1,5 +1,9 @@
 package ca.yorku.cmg.lob.stockexchange.tradingagent;
 
+import ca.yorku.cmg.lob.stockexchange.StockExchange;
+import ca.yorku.cmg.lob.stockexchange.events.NewsBoard;
+import ca.yorku.cmg.lob.trader.Trader;
+
 public class TradingAgentRetail extends TradingAgent{
 
 }
