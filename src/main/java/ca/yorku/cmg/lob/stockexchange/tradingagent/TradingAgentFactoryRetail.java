e652a94789ff76c39e8f806d147be2e24211300c
Danielk222 <dk22@my.yorku.ca>
2025-03-19 17:04:21 -0400

    Create TradingAgentFactoryRetail.java

---
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/tradingagent/TradingAgentFactoryRetail.java
@@ -0,0 +1,16 @@
+package
+
+import ca.yorku.cmg.lob.stockexchange.StockExchange;
+import ca.yorku.cmg.lob.stockexchange.events.NewsBoard;
+import ca.yorku.cmg.lob.trader.Trader;
+
+public class TradingAgentFactoryRetail implements TradingAgentFactory {
+
+    public TradingAgent createConservativeAgent() {
+        return new RetailTradingAgent(new ConservativeTradingStrategy());
+    }
+
+    public TradingAgent createAggressiveAgent() {
+        return new RetailTradingAgent(new AggressiveTradingStrategy());
+    }
+}
