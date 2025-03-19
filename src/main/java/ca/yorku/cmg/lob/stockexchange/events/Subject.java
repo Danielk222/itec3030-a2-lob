d7c3c9d1a4995f50440aca6d91c980cb213ae522
Danielk222 <dk22@my.yorku.ca>
2025-03-18 23:28:40 -0400

    Update Subject.java

--- src/main/java/ca/yorku/cmg/lob/stockexchange/events/Subject.java
+++ src/main/java/ca/yorku/cmg/lob/stockexchange/events/Subject.java
@@ -1,1 +1,15 @@
-
+public abstract class Subject {
+  
+  public void registerObserver(INewsobserver o) {
+		observers.add(o)
+	}
+	 public void removeObserver(INewsObserver o) {
+        observers.remove(o);
+	}
+		 
+	public void notifyObservers(Event event) {
+		for (INewsObserver o: observers) {
+			observer.update(event)
+		}
+	} 
+}
